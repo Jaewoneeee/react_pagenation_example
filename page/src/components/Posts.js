475e3432408eaafeb28b230443bda9380dd5d705
jaewoneeee <chou60400@naver.com>
2022-06-21 15:57:47 +0900

    pagenation example

---
+++ page/src/components/Posts.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Pagination from "./Pagination";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  console.log(posts)
+
+  return (
+    <Layout>
+      <header>
+        <h1>게시물 목록</h1>
+      </header>
+
+      <main>
+        {posts.slice(offset, offset + limit).map(({ id, title, body }) => (
+          <article key={id}>
+            <h3>
+              {id}. {title}
+            </h3>
+            <p>{body}</p>
+          </article>
+        ))}
+      </main>
+
+      <footer>
+        <Pagination
+          total={posts.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
+    </Layout>
+  );
+}
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+export default Posts;
